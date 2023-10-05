02fd042afeb9072ea8254c68340200a93c202c8d
Marki <git@marki-online.net>
2023-10-05 18:48:14 +0200

    Fix GCC8+ warning: -Wstringop-truncation (warning: ‘strncpy’ output may be truncated copying 159 bytes from a string of length 787)

--- mls_stat.c
+++ mls_stat.c
@@ -311,7 +311,10 @@ void AddStat(char *A, char *S, char *D, char *M, long sQ, long sN) {
 /* ***** myCopy ********************************************************** */
 void myCopy(char *dst, char *src, size_t max) {
   // safe copy - will add \0 to end of destination region
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstringop-truncation"
   strncpy(dst,src,max);
+#pragma GCC diagnostic pop
   dst[max-1]='\0';
 }
 
